f33bc526ea5a75d921f33c5e6d9147cc8b9e7759
HugoWeb23 <hugohourriez@live.be>
2019-10-13 16:50:48 +0200

    Améliorations

--- admin/newquestion.php
+++ admin/newquestion.php
@@ -4,13 +4,18 @@
 <head>
 
 
-	<link rel="stylesheet" href="css/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="css/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="/css/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="/styles.css">
+<script src="/css/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <title>Administration</title>
 
 
 </head>
+<body id="LoginForm">
+<div class="container">
+	<div class="login-form">
+	<div class="main-div3">
 <?php
 
 require('../config.php');
@@ -25,22 +30,46 @@ $theme = ($_POST['theme']);
 $failure = false;
 
 if(strlen($intitule_question) < 1){
-echo 'Il faut saisir un intitulé de question ! <br>';
+	echo "
+		<div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\">
+	Il faut saisir un intitulé de question !
+		<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+			<span aria-hidden=\"true\">&times;</span>
+		</button>
+	</div>";
 $failure = true;
 		 }
 
 	if(strlen($question) < 1){
-echo 'Il faut saisir une question ! <br>';
+		echo "
+			<div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\">
+			Il faut saisir une question !
+			<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+				<span aria-hidden=\"true\">&times;</span>
+			</button>
+		</div>";
 $failure = true;
 		   }
 
 	if(strlen($reponse) < 1){
-	echo 'Il faut saisir une réponse ! <br>';
+		echo "
+	    <div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\">
+	    Il faut saisir une réponse !
+	    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+	      <span aria-hidden=\"true\">&times;</span>
+	    </button>
+	  </div>";
   $failure = true;
 		 		}
 
 				if ($theme == 0){
-				echo 'Il faut choisir un thème !';
+					echo "
+				    <div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\">
+				    Il faut choisir un thème !
+				    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+				      <span aria-hidden=\"true\">&times;</span>
+				    </button>
+				  </div>";
 			  $failure = true;
 					 		}
 
@@ -48,9 +77,15 @@ $failure = true;
 	if($failure == false){
 
 $req = $bdd->prepare('INSERT INTO questions(question, reponse, id_theme, intitule_question) VALUES(:question, :reponse, :id_theme, :intitule_question)');
-$req->execute(array('question' => $question, 'reponse' => $reponse, 'id_theme' => $theme, 'intitule_question' => $intitule_question));
+$req->execute(array('question' => mb_convert_case($question, MB_CASE_LOWER, "UTF-8"), 'reponse' => mb_convert_case($reponse, MB_CASE_LOWER, "UTF-8"), 'id_theme' => $theme, 'intitule_question' => $intitule_question));
 
-echo 'Question créée !';
+echo "
+	<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">
+	La question a été créée !
+	<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+		<span aria-hidden=\"true\">&times;</span>
+	</button>
+</div>";
 
 }
 
@@ -61,7 +96,6 @@ echo 'Question créée !';
 }
 
  ?>
-<body>
 <?php
 
 $req2 = $bdd->query('SELECT * FROM questions ORDER BY id DESC LIMIT 1');
@@ -69,17 +103,24 @@ $req2 = $bdd->query('SELECT * FROM questions ORDER BY id DESC LIMIT 1');
 $selected = $req2->fetch();
 
  ?>
+ <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="/index.php">Accueil</a></li>
+		 <li class="breadcrumb-item"><a href="/admin/">Administration</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Créer une question</li>
+  </ol>
+</nav>
 <h1>Créer une nouvelle question</h1> <br />
 <form method='post' action="">
 <p>Intitulé de la question:</p>
-<p><input type="text" name="intitule_question" value="<?php echo $selected['intitule_question']; ?>" size="50"></input></p>
+<p><input class="form-control form-control-lg" type="text" name="intitule_question" value="<?php echo $selected['intitule_question']; ?>" size="50"></input></p>
 <p>Question:</p>
-<p><input type="text" name="question" size="50"></input></p>
+<p><input class="form-control form-control-lg" type="text" name="question" size="50"></input></p>
 
 <p>Réponse:</p>
-<p><input type="text" name="reponse" size="50"></input></p>
-<p>Thème:</p>
-<select name="theme">
+<p><input class="form-control form-control-lg" type="text" name="reponse" size="50"></input></p>
+<p>Thème:  <a href="/admin/newtheme.php">Créer un nouveau thème</a></p>
+<select class="form-control form-control-lg" name="theme">
 	  <?php
 
   $reponse = $bdd->query('SELECT * FROM themes');
@@ -98,11 +139,14 @@ $reponse->CloseCursor();
 
 ?>
 
-</select> <a href="/admin/newtheme.php">Créer un nouveau thème</a>
-<p><button type="submit" class="bouton" name="creer">Créer une question</button></p>
+</select>
+<br />
+<p><button class="btn btn-success btn-lg" type="submit" class="bouton" name="creer">Créer une question</button></p>
 </form>
-<br>
-<p><a href="/admin/">Retour à l'accueil</a></p>
+
+</div>
+</div>
+</div>
 </body>
 
 </html>
--- admin/newtheme.php
+++ admin/newtheme.php
@@ -4,13 +4,18 @@
 <head>
 
 
-	<link rel="stylesheet" href="css/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="css/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="/css/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="/styles.css">
+	<script src="/css/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <title>Administration</title>
 
 
 </head>
+<body id="LoginForm">
+<div class="container">
+	<div class="login-form">
+	<div class="main-div3">
 <?php
 
 require('../config.php');
@@ -22,7 +27,13 @@ $theme = ($_POST['theme']);
 $failure = false;
 
 	if(strlen($theme) < 2){
-echo 'Il faut saisir un thème !';
+		echo "
+			<div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\">
+			Il faut saisir un nom de thème !
+			<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+				<span aria-hidden=\"true\">&times;</span>
+			</button>
+		</div>";
 $failure = true;
 		   }
 
@@ -32,7 +43,13 @@ $failure = true;
 $req = $bdd->prepare('INSERT INTO themes(theme) VALUES(:theme)');
 $req->execute(array('theme' => $theme));
 
-echo 'Thème créé !';
+echo "
+	<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">
+	Le thème a été créé !
+	<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+		<span aria-hidden=\"true\">&times;</span>
+	</button>
+</div>";
 
 }
 
@@ -51,17 +68,24 @@ echo 'Le thème est : '.$theme.' ! ';
 
 
  ?>
- <body>
-
+ <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="/index.php">Accueil</a></li>
+		 <li class="breadcrumb-item"><a href="/admin/">Administration</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Créer un thème</li>
+  </ol>
+</nav>
 <h1>Créer un nouveau thème</h1> <br />
 <form method='post' action="">
 <p>Nouveau thème:</p>
-<p><input type="text" name="theme"></input></p>
+<p><input class="form-control form-control-lg" type="text" name="theme"></input></p>
 
-<p><button type="submit" class="bouton" name="creer">Créer un thème</button></p>
+<p><button class="btn btn-success btn-lg" type="submit" class="bouton" name="creer">Créer un thème</button></p>
 </form>
 <br>
-<p><a href="/admin/">Retour à l'accueil</a></p>
+</div>
+</div>
+</div>
 </body>
 
 </html>
