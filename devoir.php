61a57defefc652c289a29765550d21df08c0d876
HugoWeb23 <hugohourriez@live.be>
2019-10-20 21:44:52 +0200

    Devoir du 23/10/19

---
+++ devoir.php
@@ -0,0 +1,39 @@
+<h1>Exercice 1</h1>
+<br />
+<?php
+
+$nombre = 10;
+
+while ($nombre >= 0) {
+
+echo $nombre;
+echo '<br />';
+
+$nombre--;
+
+}
+
+ ?>
+<br />
+
+<h1>Exercice 2</h1>
+<br />
+
+<?php
+
+
+$a = 'oui';
+$b = 'non';
+$i = 1;
+
+while ($i <= 5) {
+
+echo $a;
+echo '<br />';
+echo $b;
+echo '<br />';
+$i++;
+
+}
+
+ ?>
